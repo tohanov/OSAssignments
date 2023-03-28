d3f320bf31e50b4805627c3b98b4c8113e71631b
tohanov <nope>
2023-03-28 19:32:33 +0300

    assignment 1 task 2

---
+++ user/memsize_test.c
@@ -0,0 +1,18 @@
+#include "kernel/types.h"
+// #include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+	printf("original process size = %d\n", memsize());
+
+	void *allocatedBytes = malloc(20000);
+	
+	printf("process size after malloc() = %d\n", memsize());
+
+	free(allocatedBytes);
+	
+	printf("process size after free() = %d\n", memsize());
+	exit(0);
+}
