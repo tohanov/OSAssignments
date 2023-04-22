ce51113849f5268c70719fca8c0cddf78491653f
tohanov <nope>
2023-04-22 17:24:02 +0000

    task 2.1

--- kernel/kthread.h
+++ kernel/kthread.h
@@ -50,10 +50,31 @@ struct trapframe {
   /* 280 */ uint64 t6;
 };
 
+// Per-CPU state.
+struct cpu {
+  struct kthread *kernel_thread;          // The process running on this cpu, or null.
+  struct context context;     // swtch() here to enter scheduler().
+  int noff;                   // Depth of push_off() nesting.
+  int intena;                 // Were interrupts enabled before push_off()?
+};
+
+extern struct cpu cpus[NCPU];
+
+enum thread_state { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
+
 struct kthread
 {
+	struct spinlock lock;
+	enum thread_state state;        // Process state
+	void *chan;                  // If non-zero, sleeping on chan
+	uint64 kstack;                // Virtual address of kernel stack
+	int killed;                  // If non-zero, have been killed
+	int xstate;                  // Exit status to be returned to parent's wait
+	int thread_id;                     // Process ID
 
-  uint64 kstack;                // Virtual address of kernel stack
+	struct proc *process;         // Parent process
+	uint64 kstack;               // Virtual address of kernel stack
+	struct context context;      // swtch() here to run process
 
-  struct trapframe *trapframe;  // data page for trampoline.S
+	struct trapframe *trapframe;  // data page for trampoline.S
 };
--- kernel/proc.h
+++ kernel/proc.h
@@ -20,41 +20,31 @@ struct context {
   uint64 s11;
 };
 
-// Per-CPU state.
-struct cpu {
-  struct proc *proc;          // The process running on this cpu, or null.
-  struct context context;     // swtch() here to enter scheduler().
-  int noff;                   // Depth of push_off() nesting.
-  int intena;                 // Were interrupts enabled before push_off()?
-};
-
-extern struct cpu cpus[NCPU];
-
 enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
 // Per-process state
 struct proc {
-  struct spinlock lock;
+	struct spinlock lock;
 
-  // p->lock must be held when using these:
-  enum procstate state;        // Process state
-  void *chan;                  // If non-zero, sleeping on chan
-  int killed;                  // If non-zero, have been killed
-  int xstate;                  // Exit status to be returned to parent's wait
-  int pid;                     // Process ID
+	struct spinlock thread_ids_lock;
+	int next_thread_id;
 
-  struct kthread kthread[NKT];        // kthread group table
-  struct trapframe *base_trapframes;  // data page for trampolines
+	// p->lock must be held when using these:
+	enum procstate state;        // Process state
+	int killed;                  // If non-zero, have been killed
+	int xstate;                  // Exit status to be returned to parent's wait
+	int pid;                     // Process ID
 
-  // wait_lock must be held when using this:
-  struct proc *parent;         // Parent process
+	struct kthread kthread[NKT];        // kthread group table
+	struct trapframe *base_trapframes;  // data page for trampolines
 
-  // these are private to the process, so p->lock need not be held.
-  uint64 kstack;               // Virtual address of kernel stack
-  uint64 sz;                   // Size of process memory (bytes)
-  pagetable_t pagetable;       // User page table
-  struct context context;      // swtch() here to run process
-  struct file *ofile[NOFILE];  // Open files
-  struct inode *cwd;           // Current directory
-  char name[16];               // Process name (debugging)
+	// wait_lock must be held when using this:
+	struct proc *parent;         // Parent process
+
+	// these are private to the process, so p->lock need not be held.
+	uint64 sz;                   // Size of process memory (bytes)
+	pagetable_t pagetable;       // User page table
+	struct file *ofile[NOFILE];  // Open files
+	struct inode *cwd;           // Current directory
+	char name[16];               // Process name (debugging)
 };
